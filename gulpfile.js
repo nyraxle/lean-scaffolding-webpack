a88efe96950133b16451cb40e0548e640738cf35
Eduardo Arnold <eduardo.arnold@outlook.com>
2016-03-22 11:05:51 -0300

    chore(gulp): add webpack build (PRD) task

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,36 @@
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var jshint = require('gulp-jshint');
-var eslint = require('gulp-eslint');
+
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+var webpackConfig = require("./webpack.config.js");
 
 gulp.task('default', () => {
   gutil.log('Gulp Running');
 });
 
-/*
- * Code Lint
- */
-gulp.task('lint:jshint', () => {
-  return gulp.src('source/js/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
 
-gulp.task('lint:eslint', () => {
-  return gulp.src('source/js/**/*.js')
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-    .pipe(eslint.results((results) => {
-      console.log('Total Results: ' + results.length);
-      console.log('Total Warnings: ' + results.warningCount);
-      console.log('Total Errors: ' + results.errorCount);
-    }));
+gulp.task("webpack:build", function(callback) {
+	// modify some webpack config options
+	var myConfig = Object.create(webpackConfig);
+	myConfig.plugins = myConfig.plugins.concat(
+		new webpack.DefinePlugin({
+			"process.env": {
+				// This has effect on the react lib size
+				"NODE_ENV": JSON.stringify("production")
+			}
+		}),
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin()
+	);
 
+	// run webpack
+	webpack(myConfig, function(err, stats) {
+		if(err) throw new gutil.PluginError("webpack:build", err);
+		gutil.log("[webpack:build]", stats.toString({
+			colors: true
+		}));
+		callback();
+	});
 });
