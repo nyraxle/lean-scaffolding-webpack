c1eb0549d781968bdf2da0cc07e1f135db161cf7
Eduardo Arnold <eduardo.arnold@outlook.com>
2016-03-23 10:42:37 -0300

    chore(lint): add eslint and implement eslint task

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,40 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./webpack.config.js');
 
+const eslint = require('gulp-eslint');
+
+const paths = {
+  source: './src/',
+  dist: './dist'
+};
+paths.srcJS = paths.source + 'js/**/*.js';
+
+
 gulp.task('default', () => {
   gutil.log('Gulp Running');
 });
 
-gulp.task('webpack:build', (callback) => {
+/*
+  Lint
+*/
+gulp.task('lint:eslint', (callback) => {
+  return gulp.src(paths.srcJS)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(eslint.result((result) => {
+      var fileLog = gutil.colors.blue('ESLint for file:', result.filePath);
+      var messageLog = gutil.colors.white.underline('# Messages:', result.messages.length);
+      var warningLog = gutil.colors.white.underline('# Warnings:', result.warningCount);
+      var errorLog = gutil.colors.white.underline('# Errors:', result.errorCount);
+      gutil.log(fileLog + messageLog + warningLog + errorLog);
+    }));
+});
+
+/*
+  WebPack
+*/
+gulp.task('webpack:build', ['lint:eslint'], (callback) => {
   'use strict';
 
 	let buildConfig = Object.create(webpackConfig);
@@ -40,7 +69,7 @@ gulp.task('webpack:build', (callback) => {
 });
 
 
-gulp.task('webpack:build-debug', (callback) => {
+gulp.task('webpack:build-debug', ['lint:eslint'], (callback) => {
   let debugConfig = Object.create(webpackConfig);
   debugConfig.devtool = 'sourcemap';
   debugConfig.debug = true;
